09bfc0577426369d9fa5ed752dfe392c488c6b4a
Mihail <misagfo@gmail.com>
2021-04-23 15:33:00 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Input } from '@material-ui/core'
+import Button from '@material-ui/core/Button';
+
+import data from './data.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [value, setValue] = React.useState(data.date)
+const [result, setResult] = React.useState([])
+
+
+const changeData = () => {
+  return value.push(result)
+}
+
+
+const showData = () => {
+  return setValue([...value])
+}
+
+return (
+  <div className="App">
+     <TableContainer component={Paper}>
+          <Table size="small">
+                <TableBody>
+           {value.map((row, index) => (
+              <TableRow key={index}>
+                 <TableCell >{row}</TableCell>
+              </TableRow>
+          ))}
+        </TableBody>
+             </Table>
+        </TableContainer>
+
+    <Input onChange={(e) => setResult(e.target.value)} className="input" type="number" placeholder="AddNumber"></Input>
+    <Button onClick={() => changeData()} variant="contained" color="primary">Add</Button>
+    <br/>
+    <br/>
+    <Button onClick={() => showData()} variant="contained">Change</Button>
+  </div>
+   
+  )
 }
 
 export default App;
